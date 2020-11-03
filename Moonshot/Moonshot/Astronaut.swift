a81b28cf4ed4d9dc91bfa5468a398d5e21f4a65b
Manuel Teixeira <mteixeira@cocus.com>
2020-11-03 08:39:30 +0000

    Day 40 - Project 8, part 2

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Manuel Teixeira on 03/11/2020.
+//  Copyright © 2020 Manuel Teixeira. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,39 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Manuel Teixeira on 03/11/2020.
+//  Copyright © 2020 Manuel Teixeira. All rights reserved.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+}
