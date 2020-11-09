3f387bea26fb4d8c67d4df1590a9eafbb9ed0135
Manuel Teixeira <mteixeira@cocus.com>
2020-11-09 08:32:55 +0000

    Day 43 - Project 9, part 1

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Manuel Teixeira on 09/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
