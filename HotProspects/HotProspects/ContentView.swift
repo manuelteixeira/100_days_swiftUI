4a0afbdda5eecb98aa2545d903f19e7bfcebbb80
Manuel Teixeira <teixeirasmanuel@gmail.com>
2021-02-10 08:56:10 +0000

    Day 81 - Project 16, part 3

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -6,90 +6,20 @@
 //
 
 import SwiftUI
-
-class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-
-    var body: some View {
-        Text(user.name)
-    }
-}
-
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
-}
-
-class DelayedUpdater: ObservableObject {
-    @Published var value = 0
-    
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
-                self.value += 1
-            }
-        }
-    }
-}
+import UserNotifications
+import SamplePackage
 
 struct ContentView: View {
-    @State private var selectedTab = 0
-    @ObservedObject var updater = DelayedUpdater()
-
-    var body: some View {
-        Text("Hello, World")
-            .onAppear {
-                fetchData(from: "https://www.apple.com") { result in
-                    switch result {
-                    case let .success(str):
-                        print(str)
-                    case let .failure(error):
-                        switch error {
-                        case .badURL:
-                            print("Bad URL")
-                        case .requestFailed:
-                            print("Network problems")
-                        case .unknown:
-                            print("Unknown error")
-                        }
-                    }
-                }
-            }
-        
-        Text("Value is: \(updater.value)")
-        
-        
+    let possibleNumbers = Array(1...60)
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
     }
 
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            DispatchQueue.main.async {
-                if let data = data {
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    completion(.failure(.requestFailed))
-                } else {
-                    completion(.failure(.unknown))
-                }
-            }
-        }.resume()
+    var body: some View {
+        Text(results)
     }
 }
 
