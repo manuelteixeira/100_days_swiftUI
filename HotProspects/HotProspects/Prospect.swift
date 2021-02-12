014dc8a08229803ffcb4166c619978fc79ac76ab
Manuel Teixeira <teixeirasmanuel@gmail.com>
2021-02-12 09:32:58 +0000

    Day 82 - Project 16, part 4

---
+++ HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Manuel Teixeira on 12/02/2021.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        self.people = []
+    }
+}
---
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,70 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Manuel Teixeira on 12/02/2021.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    @EnvironmentObject var prospects: Prospects
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    var filterProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(filterProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+                .navigationBarTitle(title)
+                .navigationBarItems(trailing: Button(action: {
+                    let prospect = Prospect()
+                    prospect.name = "Paul Hudson"
+                    prospect.emailAddress = "paul@hackingwithswift.com"
+                    self.prospects.people.append(prospect)
+                }, label: {
+                    Image(systemName: "qrcode.viewfinder")
+                    Text("Scan")
+                }))
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
